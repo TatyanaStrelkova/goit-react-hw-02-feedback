085e057b8d6e5403e6329cde35a1806e8c2af481
Tatyana <tatyana_strelkova@i.ua>
2022-07-27 12:39:42 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-export const App = () => {
-  return (
+import React, { Component } from "react";
+
+export class App extends Component  {
+/*   return (
     <div
       style={{
         height: '100vh',
@@ -10,7 +12,36 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
     </div>
-  );
+  ); */
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  contGoodStates = () => this.setState((prevState) => { return {
+    good: prevState.good + 1
+  }})
+
+  contNeutralStates = () => this.setState((prevState) => {})
+
+  contBadStates = () => {}
+
+  render() {
+    return <div>
+    <hi>Please leave feedback</hi>
+    <button>Good</button>
+    <button>Neutral</button>
+    <button>Bad</button>
+    <ul>Statistics</ul>
+    <li>Good</li>
+    <li>Neutral</li>
+    <li>Bad</li>
+    <li>Total</li>
+    <li>Positive feedback</li>
+    </div>
+  }
+
 };
